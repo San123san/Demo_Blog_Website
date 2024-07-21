9c709c77546dd0bdedfea8173293de364c327c80
San123san <s35102751@gmail.com>
2024-07-21 23:21:36 +0530

    sixth commit

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,8 @@ import { registration } from "../models/registration.models.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        // const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.headers["authorization"].split(" ")[1]
         console.log("Token:", token);
         
         // console.log(token);
