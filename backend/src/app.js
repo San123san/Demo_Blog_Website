ba4c5b558638b2b9c878cd9377c062a307887c96
San123san <147592258+San123san@users.noreply.github.com>
2024-07-21 16:42:34 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: 'https://669ce7d99b863d05044adc98--grand-truffle-3e9d71.netlify.app/',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
