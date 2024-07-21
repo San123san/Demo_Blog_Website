fbb8be606e61d7b70146e48f4e9f4ede0d17ab66
San123san <s35102751@gmail.com>
2024-07-21 19:29:33 +0530

    third commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api/v1/users': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/upload': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/share': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/total': 'https://demo-blog-website-dwt4.onrender.com',
-      // '/api/v1/users': 'http://localhost:8000',
-      // '/api/v1/upload': 'http://localhost:8000',
-      // '/api/v1/share': 'http://localhost:8000',
-      // '/api/v1/total': 'http://localhost:8000',
     },
   },
   plugins: [react()],
