20184f1ff7be9268d89b482221b314fb3fe61787
San123san <147592258+San123san@users.noreply.github.com>
2024-07-21 18:18:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      '/api/v1/users': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/upload': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/share': 'https://demo-blog-website-dwt4.onrender.com',
-      '/api/v1/total': 'https://demo-blog-website-dwt4.onrender.com',
+    // proxy:{
+    //   '/api/v1/users': 'https://demo-blog-website-dwt4.onrender.com',
+    //   '/api/v1/upload': 'https://demo-blog-website-dwt4.onrender.com',
+    //   '/api/v1/share': 'https://demo-blog-website-dwt4.onrender.com',
+    //   '/api/v1/total': 'https://demo-blog-website-dwt4.onrender.com',
     },
   },
   plugins: [react()],
